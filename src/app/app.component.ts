c3a9a5b09e4a9b64b389e5f996f8de226da3191a
WaleedAhmad448 <w.ahmed@kitsys.co>
2025-05-27 23:26:06 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,3 @@
-/*
-import { Component } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
-@Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html'
-})
-export class AppComponent {
-
-    menuMode = 'static';
-
-    constructor(private primengConfig: PrimeNGConfig) { }
-
-    ngOnInit() {
-        this.primengConfig.ripple = true;
-        document.documentElement.style.fontSize = '14px';
-    }
-}
-*/
-
-
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
 import { LayoutService } from './layout/service/app.layout.service';
